e92be2609909c86c539c67c5d72ba5d847f58744
Roman Polulikh <polilikh.r@gmail.com>
2022-01-26 22:27:34 +0200

    fixed codeStyle

--- src/main/java/com/example/bookstore/dao/BookDao.java
+++ src/main/java/com/example/bookstore/dao/BookDao.java
@@ -14,22 +14,22 @@ public interface BookDao extends JpaRepository<Book, Long> {
 
     Book findFirstByAuthorNameOrderByPublishedAmountDesc(String name);
 
-    @Query(value = "select b.id, b.book_name, b.author_id, "
-            + "b.published_amount, max(b.sold_amount) as sold_amount "
-            + "FROM books b join authors a on b.author_id = a.id "
-            + "where a.name like :name group by a.id", nativeQuery = true)
+    @Query(value = "SELECT b.id, b.book_name, b.author_id, "
+            + "b.published_amount, max(b.sold_amount) AS sold_amount "
+            + "FROM books b JOIN authors a ON b.author_id = a.id "
+            + "WHERE UPPER(a.name) LIKE UPPER(:name) GROUP BY a.id", nativeQuery = true)
     List<Book> getMostSellingByAuthorPartial(String name);
 
-    @Query(value = "select b.id, b.book_name, b.author_id, "
-            + "max(b.published_amount) as published_amount, b.sold_amount "
-            + "FROM books b join  authors a on b.author_id = a.id "
-            + "where a.name like :name group by a.id", nativeQuery = true)
+    @Query(value = "SELECT b.id, b.book_name, b.author_id, "
+            + "max(b.published_amount) AS published_amount, b.sold_amount "
+            + "FROM books b JOIN  authors a ON b.author_id = a.id "
+            + "WHERE UPPER(a.name) LIKE UPPER(:name) GROUP BY a.id", nativeQuery = true)
     List<Book> getMostPublishedByAuthorPartial(String name);
 
-    @Query(value = "select * from books b join (select max(b2.sold_amount/b2.published_amount) "
-            + "as max from books b2 join authors a2 on b2.author_id = a2.id "
-            + "where a2.name like :name group by a2.id) as ss "
-            + "on b.sold_amount/b.published_amount = ss.max join authors a "
-            + "on a.id = b.author_id", nativeQuery = true)
+    @Query(value = "SELECT * FROM books b JOIN (SELECT max(b2.sold_amount/b2.published_amount) "
+            + "AS max FROM books b2 JOIN authors a2 ON b2.author_id = a2.id "
+            + "WHERE UPPER(a2.name) LIKE UPPER(:name) GROUP BY a2.id) AS ss "
+            + "ON b.sold_amount/b.published_amount = ss.max JOIN authors a "
+            + "ON a.id = b.author_id", nativeQuery = true)
     List<Book> getMostSuccessfulByAuthorPartial(String name);
 }
